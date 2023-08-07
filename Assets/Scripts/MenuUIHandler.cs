9240fcfdc954e0605d6ac70a6f86def218f4c154
Dalahan Thalstan <129230657+DalahanThalstan@users.noreply.github.com>
2023-08-07 17:59:20 -0400

    - removed unneeded Start() method from MenuUIHandler

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public void StartGame() {
         SceneManager.LoadScene(1); // Load the "main" scene
